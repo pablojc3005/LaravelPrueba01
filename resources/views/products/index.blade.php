ef173089bf45f6637ee1c35c7076dc4550322a33
pablojc3005 <pablojc3005@gmail.com>
2024-10-10 12:35:59 -0500

    mi primer commit en laravel

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,86 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Lista de Productos</h1>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.3/css/dataTables.bootstrap5.css">
+@endsection
+
+@section('content')
+   
+<div class="row justify-content-center mt-3">
+    <div class="col-md-12">
+
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success" role="alert">
+                {{ $message }}
+            </div>
+        @endif
+
+        <div class="card">
+            <div class="card-header">Productos</div>
+            <div class="card-body">
+                <a href="{{ route('products.create') }}" class="btn btn-success btn-sm my-2"><i class="bi bi-plus-circle"></i> Nuevo Producto</a>
+                <table class="table table-striped table-bordered" id="productos">
+                    <thead>
+                      <tr>
+                        <th scope="col">N#</th>
+                        <th scope="col">Codigo</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Descripcion</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($products as $product)
+                        <tr>
+                            <th scope="row">{{ $loop->iteration }}</th>
+                            <td>{{ $product->code }}</td>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->quantity }}</td>
+                            <td>{{ $product->price }}</td>
+                            <td>
+                                <form action="{{ route('products.destroy', $product->id) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <a href="{{ route('products.show', $product->id) }}" class="btn btn-warning btn-sm"><i class="bi bi-eye"></i> Ver</a>
+
+                                    <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary btn-sm"><i class="bi bi-pencil-square"></i> Editar</a>   
+
+                                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Do you want to delete this product?');"><i class="bi bi-trash"></i> Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @empty
+                            <td colspan="6">
+                                <span class="text-danger">
+                                    <strong>No se encontro ningun Producto!</strong>
+                                </span>
+                            </td>
+                        @endforelse
+                    </tbody>
+                  </table>
+
+                
+
+            </div>
+        </div>
+    </div>    
+</div>
+
+@stop
+
+@section('js')
+    <script src="https://cdn.datatables.net/2.1.3/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.1.3/js/dataTables.bootstrap5.js"></script>
+    <script>
+         new DataTable('#productos');
+    </script>
+@endsection
---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,65 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+
+<div class="row justify-content-center mt-3">
+    <div class="col-md-8">
+
+        <div class="card">
+            <div class="card-header">
+                <div class="float-start">
+                    Information del Producto
+                </div>
+                <div class="float-end">
+                    <a href="{{ route('products.index') }}" class="btn btn-primary btn-sm">&larr; Regresar</a>
+                </div>
+            </div>
+            <div class="card-body">
+
+                    <div class="row">
+                        <label for="code" class="col-md-4 col-form-label text-md-end text-start"><strong>Codigo:</strong></label>
+                        <div class="col-md-6" style="line-height: 35px;">
+                            {{ $product->code }}
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <label for="name" class="col-md-4 col-form-label text-md-end text-start"><strong>Nombre:</strong></label>
+                        <div class="col-md-6" style="line-height: 35px;">
+                            {{ $product->name }}
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <label for="quantity" class="col-md-4 col-form-label text-md-end text-start"><strong>Cantidad:</strong></label>
+                        <div class="col-md-6" style="line-height: 35px;">
+                            {{ $product->quantity }}
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <label for="price" class="col-md-4 col-form-label text-md-end text-start"><strong>Precio:</strong></label>
+                        <div class="col-md-6" style="line-height: 35px;">
+                            {{ $product->price }}
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <label for="description" class="col-md-4 col-form-label text-md-end text-start"><strong>Descripcion:</strong></label>
+                        <div class="col-md-6" style="line-height: 35px;">
+                            {{ $product->description }}
+                        </div>
+                    </div>
+        
+            </div>
+        </div>
+    </div>    
+</div>
+    
+@endsection
